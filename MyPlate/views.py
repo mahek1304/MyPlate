a81449aad006e0d16cfc4856d385aab189c10c80
Mahek Chhabria <65598464+mahek1304@users.noreply.github.com>
2021-07-27 21:11:58 +0530

    Update views.py

--- MyPlate/views.py
+++ MyPlate/views.py
@@ -3,8 +3,6 @@ from django.views.generic import TemplateView
 class HomePage(TemplateView):
     template_name = 'index.html'
 
-class LoggedIn(TemplateView):
-    template_name = 'loggedin.html'
 
     
 
